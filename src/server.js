0fb7f3544852fb748b2c01a6a812b89e0f0e2d80
Abbet Wang <abbetwang@gmail.com>
2019-04-07 14:21:35 +0800

    add session

--- src/server.js
+++ src/server.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const passport = require('passport')
+var session = require("express-session")
+
 const LocalStrategy = require('passport-local').Strategy
 const app = express()
 
 passport.use('local',new LocalStrategy(
     (username, password, done) => {
         if(username == '23'){
-        console.log(username+ ':' + password)
-          return done(null, {username: username,password:password});
+       // console.log(username+ ':' + password)
+          return done(null, {id:'1',username: username,password:password});
         }else{
             return done(err);
             //return done(null, false, { message: 'Incorrect username.' });
@@ -15,26 +17,28 @@ passport.use('local',new LocalStrategy(
       }
 ))
 passport.serializeUser(function(user, done) {
-    console.table(user)
+  //  console.table(user)
     done(null, user);
 })
   
 passport.deserializeUser(function(id, done) {
       console.log(id)
-      done(err, id);
+      done(null, {id:'1',username:'1212',password:'122'});
 })
+app.use(session({ secret: "cats" }));
 app.use(passport.initialize())
 app.use(passport.session())
 app.get('/login',
 passport.authenticate('local'),
 (req,res)=>{
-    console.log(req.user.username)
-    console.table(req.session)
+   // console.log(req.user.username)
+  //  console.table(req.session)
     res.json({message: 'success'})
 })
 app.get('/',(req,res)=>{
     res.json({
-        message: req.ip
+        message: req.ip,
+        user: req.user
     })
 })
 app.listen(3000,()=>{
