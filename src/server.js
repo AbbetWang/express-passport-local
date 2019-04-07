22ab574d445eb8cabaf4c45a05ff8ad8f09cd8e8
Abbet Wang <abbetwang@gmail.com>
2019-04-07 22:51:32 +0800

    test bcrypt

--- src/server.js
+++ src/server.js
@@ -47,9 +47,9 @@ passport.authenticate('local'),
   //  console.table(req.session)
     res.json({message: 'success'})
 })
-app.get('/', (req,res)=>{
+app.get('/',async (req,res)=>{
 
-   // const match = await checkUser(req.user,'1234')
+    const match = await checkUser(req.user,'1234')
     console.log(match)
     res.json({
         message: req.ip,
